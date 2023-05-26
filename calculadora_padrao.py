f49519ad4f278beaa83d372847b416b9e53dafe9
henrique-bees <riquedemoraes424@gmail.com>
2023-05-26 00:01:08 -0300

    commit inicial

---
+++ calculadora_padrao.py
@@ -0,0 +1,29 @@
+# calculadora em python
+print('''Para calcular escreva a operação separando os termos com espaço
+exemplo: 5 * 5
+use o sinal padrão para cada operação matemática simples
+digite "sair" para parar o programa
+''')
+while True:
+    c = input('calcule: ')
+    nt = c.split(' ')
+    lista = [str(x) for x in nt]
+    print(lista)
+
+    if lista[0] == 'sair':
+        print('desligando')
+        quit()
+    elif lista[1] == '*':
+        m = float(lista[0]) * float(lista[2])
+        print(m)
+    elif lista[1] == '+':
+        m = float(lista[0]) + float(lista[2])
+        print(m)
+    elif lista[1] == '-':
+        m = float(lista[0]) - float(lista[2])
+        print(m)
+    elif lista[1] == '/':
+        m = float(lista[0]) / float(lista[2])
+        print(m)
+    else:
+        print('houve um erro')
